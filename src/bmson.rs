29764c118db10cd75fa95ee2ab03ed93b5e094ea
Kirby0717 <niconicoyusei@gmail.com>
2025-04-07 23:16:39 +0900

    ハッシュや同値判定出来る小数を作る その1

--- src/bmson.rs
+++ src/bmson.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
+use serde_with::*;
 use serde_json::Result;
 
 /// Bmson本体
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct Bmson {
     pub version: String,
     pub info: BmsonInfo,
@@ -20,9 +21,11 @@ pub struct Bmson {
 }
 
 /// ヘッダー
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[serde_as]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct BmsonInfo {
     pub title: String,
+    #[serde(default)]
     pub subtitle: String,
     pub artist: String,
     pub subartists: Option<Vec<String>>,
@@ -31,6 +34,7 @@ pub struct BmsonInfo {
     pub mode_hint: String,
     pub chart_name: String,
     pub level: u32,
+    #[serde_as(as = "DisplayFromStr")]
     pub init_bpm: f64,
     #[serde(default = "default_judge_rank")]
     pub judge_rank: f64,
@@ -59,7 +63,7 @@ fn default_resolution() -> u32 {
 }
 
 /// 小節線イベント
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct BarLine {
     pub y: u32,
 }
@@ -67,14 +71,14 @@ pub struct BarLine {
 /// サウンドチャンネル
 ///
 /// DTMソフトのトラックと似ている
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct SoundChannel {
     pub name: String,
     pub notes: Vec<Note>,
 }
 
 /// サウンドノート
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct Note {
     /// 演奏レーン
     ///
@@ -90,21 +94,21 @@ pub struct Note {
 }
 
 /// BPM変化イベント
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct BpmEvent {
     pub y: u32,
     pub bpm: f64,
 }
 
 /// 譜面停止イベント
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct StopEvent {
     pub y: u32,
     pub duration: u32,
 }
 
 /// BGAデータ
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct Bga {
     pub bga_header: Vec<BgaHeader>,
     pub bga_events: Vec<BgaEvent>,
@@ -113,34 +117,34 @@ pub struct Bga {
 }
 
 /// 画像ファイル
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct BgaHeader {
     pub id: u32,
     pub name: String,
 }
 
 /// BGA設定イベント
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct BgaEvent {
     pub y: u32,
     pub id: u32,
 }
 
 /// スクロール速度設定イベント（beatoraja拡張）
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct ScrollEvent {
     pub y: f64,
 }
 
 /// 地雷チャンネル（beatoraja拡張）
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct MineChannel {
     pub name: String,
     pub notes: Vec<MineNote>,
 }
 
 /// 地雷ノート（beatoraja拡張）
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct MineNote {
     pub x: Option<u32>,
     pub y: u32,
@@ -148,21 +152,38 @@ pub struct MineNote {
 }
 
 /// 不可視チャンネル（beatoraja拡張）
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct KeyChannel {
     pub name: String,
     pub notes: Vec<KeyNote>,
 }
 
 /// 不可視ノート（beatoraja拡張）
-#[derive(Deserialize, Serialize, Debug, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct KeyNote {
     pub x: Option<u32>,
     pub y: u32,
 }
 
+impl std::str::FromStr for Bmson {
+    type Err = serde_json::Error;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        serde_json::from_str(s)
+    }
+}
+impl std::fmt::Display for Bmson {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            serde_json::to_string_pretty(self)
+                .map_err(|_| { std::fmt::Error })?
+        )?;
+        Ok(())
+    }
+}
 impl Bmson {
-    pub fn new(source: &str) -> Result<Bmson> {
+    pub fn parse(source: &str) -> Result<Bmson> {
         serde_json::from_str(source)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,65 @@ pub use bms::RawBms;
 /// * <https://github.com/exch-bms2/beatoraja/wiki/楽曲製作者向け資料>
 pub mod bmson;
 pub use bmson::Bmson;
+
+/// ハッシュや同値判定ができる少数
+#[derive(Clone, Default, Debug, PartialEq, Eq, Hash)]
+pub struct Decimal {
+    /// 整数部分
+    pub integer_part: i64,
+    /// 小数部分
+    pub fractional_part: u64,
+}
+
+impl From<Decimal> for f32 {
+    fn from(value: Decimal) -> Self {
+        let fractional_part = if value.fractional_part == 0 {
+            0.0
+        }
+        else {
+            value.fractional_part as f32
+                * 10.0_f32.powi(-(1 + value.fractional_part.ilog10() as i32))
+        };
+        if 0 <= value.integer_part {
+            value.integer_part as f32 + fractional_part
+        }
+        else {
+            value.integer_part as f32 - fractional_part
+        }
+    }
+}
+impl From<Decimal> for f64 {
+    fn from(value: Decimal) -> Self {
+        let fractional_part = if value.fractional_part == 0 {
+            0.0
+        }
+        else {
+            value.fractional_part as f64
+                * 10.0_f64.powi(-(1 + value.fractional_part.ilog10() as i32))
+        };
+        if 0 <= value.integer_part {
+            value.integer_part as f64 + fractional_part
+        }
+        else {
+            value.integer_part as f64 - fractional_part
+        }
+    }
+}
+impl serde_with::SerializeAs<f64> for Decimal {
+    fn serialize_as<S>(source: &f64, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        //serializer.serialize_str(s)
+        todo!()
+    }
+}
+
+impl<'de> serde_with::DeserializeAs<'de, f64> for Decimal {
+    fn deserialize_as<D>(deserializer: D) -> Result<f64, D::Error>
+        where
+            D: serde::Deserializer<'de> {
+        //let s = String::deserialize(deserializer).map_err(serde::de::Error::custom)?;
+        todo!()
+    }
+}
